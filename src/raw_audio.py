761a019355a17b119f2392b3109761b1713cd38a
ULSBAI <140907693+ulsbai@users.noreply.github.com>
2024-02-12 16:10:59 -0800

    Create raw_audio.py

---
+++ src/raw_audio.py
@@ -0,0 +1,19 @@
+import numpy
+import sounddevice
+
+DEFAULT_SAMPLE_RATE = 44100
+
+def play_freq(freq, duration, sample_rate=DEFAULT_SAMPLE_RATE):
+  t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+  
+  audio = np.sin(2 * np.pi * freq * t)
+  
+  sd.play(audio, sample_rate)
+  sd.wait()
+
+if __name__=='__main__':
+  #test
+  while True:
+    freq = input('Freq: ')
+    duration = input('Duration: ')
+    play_freq(freq, duration)
